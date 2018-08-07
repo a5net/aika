06911516c52b281dd6b22eb1702846110451c7ec
AyanGinet <ayan.ginet@nu.edu.kz>
2018-08-07 17:32:19 +0600

    movie_helper.py now uses dynamic inline messages

--- movie_helper.py
+++ movie_helper.py
@@ -11,100 +11,88 @@ class User:
     def __init__(self, chat_id):
         self.chat_id = chat_id
         self.city = None
+        self.city_id = None
+        self.movie_id = None
         self.movie_name = None
         self.cinema_name = None
+        self.cinema_id = None
 
 user_dict = {}
+city_list_for_movie = get_city_dict().keys()
 
 
-#TODO
-#city_list = get_city_list_from
+def draw_city_list():
+    markup = types.InlineKeyboardMarkup()
+    for x in city_list_for_movie:
+        markup.add(types.InlineKeyboardButton(x, callback_data="movie"))
+    return markup
+
+def draw_movie_list(city_id):
+    markup = types.InlineKeyboardMarkup()
+    movie_list = get_movie_list(city_id)
+    markup.add(types.InlineKeyboardButton("Выберите название фильма", callback_data="ignore"))
+    for x in movie_list:
+        markup.add(types.InlineKeyboardButton(x, callback_data="cinema"))
+    markup.add(types.InlineKeyboardButton("Назад к выбору города", callback_data="back_to_city"))
+    return markup
+
+def draw_cinema_list(city_id):
+    cinema_list = get_cinema_list(city_id)
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton("Выберите название кинотеатра", callback_data="ignore"))
+    for x in cinema_list:
+        markup.add(types.InlineKeyboardButton(x, callback_data="sessions"))
+    markup.add(types.InlineKeyboardButton("Назад к выбору фильма", callback_data="back_to_movie"))
+    return markup
+
+def draw_seesions_list(city_id, movie_id, cinema_id, cinema_name):
+    markup = types.InlineKeyboardMarkup()
+    sessions_list = get_session_list(city_id, movie_id, cinema_id)
+    for x in sessions_list:
+        markup.add(types.InlineKeyboardButton(x, callback_data="ignore"))
+    markup.add(types.InlineKeyboardButton(text="Купить билеты", url=get_ticket_url(cinema_name, city_id)))
+    markup.add(types.InlineKeyboardButton("Назад к выбору кинотеатра", callback_data="back_to_cinema"))
+    return markup
 
 @bot.message_handler(commands=['movie'])
-def show_city_name(message):
-    raw_city_text = message.text
-    city_id = extract_city_id(raw_city_text)
-    if city_id == 0:
-        user = User(message.chat.id)
-        user_dict[message.chat.id] = user
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        city_list_name_movie = get_city_dict()
-        city_names_movie = city_list_name_movie.keys()
-        for x in city_names_movie:
-            b = types.KeyboardButton(x)
-            markup.add(b)
-        msg = bot.send_message(message.chat.id, 'Пожалуйста, выберите название города из списка', reply_markup=markup)
-        bot.register_next_step_handler(msg, process_city_name)
-    else:
-        user = User(message.chat.id)
-        user.city = city_id
-        user_dict[message.chat.id] = user
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        movie_list = get_movie_list(city_id)
-        for x in movie_list:
-            markup.add(types.KeyboardButton(x))        
-        msg = bot.send_message(message.chat.id, 'Выберите название фильма', reply_markup=markup)
-        bot.register_next_step_handler(msg, process_movie_name)
-
-def process_city_name(message):
-    # try:
+def start_movie_helper(message):
     chat_id = message.chat.id
-    user = user_dict[chat_id]
-    city_dict = get_city_dict()
-    city_id = city_dict[message.text]
-    user.city = city_id
-    user_dict[chat_id] = user
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    movie_list = get_movie_list(city_id)
-    for x in movie_list:
-        markup.add(types.KeyboardButton(x))        
-    msg = bot.send_message(message.chat.id, 'Выберите название фильма', reply_markup=markup)
-    bot.register_next_step_handler(msg, process_movie_name)
-    # except Exception as e:
-    #     bot.reply_to(message, 'Error in process_city_name')
-
-
-def process_movie_name(message):
-    try:
-        chat_id =message.chat.id
-        user = user_dict[chat_id]
-        user.movie_name = get_movie_id(user.city, message.text)
-        user_dict[chat_id] = user
-
-        cinema_list = get_cinema_list(user.city)
-        markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-        for x in cinema_list:
-            markup.add(types.KeyboardButton(x))
-        msg = bot.send_message(chat_id, 'Выберите название кинотеатра', reply_markup=markup)
-        bot.register_next_step_handler(msg, process_cinema_name)
-    except Exception as e:
-        bot.reply_to(message, 'Error in process_movie_name')
-
-def process_cinema_name(message):
-    # try:
-    chat_id = message.chat.id
-    user = user_dict[chat_id]
-    user.cinema_name = get_cinema_id(user.city, message.text)
-    user_dict[chat_id] = user
-    sessions_list = get_session_list(user.city, user.movie_name, user.cinema_name)
-    markup = types.ReplyKeyboardMarkup(resize_keyboard=True)
-    for x in sessions_list:
-        markup.add(types.KeyboardButton(x))
-    # markup2 = types.InlneKeyboardMarkup()
-    # markup2.add(types.InlineKeyboardButton('Купить билеты', url=get_ticket_url(message.text, user.city)))
-    if(len(sessions_list) != 0):
-        bot.send_message(chat_id, movie_description(user.city, user.movie_name))
-        bot.send_message(chat_id, 'Выберите сеанс', reply_markup=markup)
-        # bot.send_message(chat_id, 'Выберите сеанс', reply_markup=markup2)
-        # bot.register_next_step_handler(msg, get_link)
-    else:
-        bot.send_message(chat_id, 'К сожалению в данном кинотеатре не показывают ваш фильм, выберите другой')
+    text = message.text
+    city_id = extract_city_id(text)
     
-    # except Exception as e:
-    #     pass    
+    if(city_id == 0):
+        markup = draw_city_list()
+        bot.send_message(chat_id, "Movie Bot", reply_markup=markup)
+    else:
+        markup = draw_movie_list(city_id)
+        bot.send_message(chat_id, "Movie Bot", reply_markup=markup)
+        user = User(chat_id)
+        user_dict[chat_id] = user
+    
+@bot.callback_query_handler(func=lambda call: True)
+def message_query_handler(call):
+    message_type = call.data
+    message = call.message.text
+    chat_id = call.message.chat.id
+    user = User(chat_id)
 
-bot.enable_save_next_step_handlers(delay=2)
-
-bot.load_next_step_handlers()
-
-bot.polling()
+    if(message_type == 'city' or message_type == 'back_to_city'):
+        markup = draw_city_list()
+        bot.edit_message_text('Movie Bot', call.from_user.id, call.message.message_id, reply_markup=markup)
+    if(message_type == 'movie' or message_type == 'back_to_movie'):
+        user.city = message
+        city_dict = get_city_dict()
+        user.city_id = city_dict[message]
+        user_dict[user.chat_id] = user
+        markup = draw_movie_list(user.city_id)
+        bot.edit_message_text('Movie Bot', call.from_user.id, call.message.message_id, reply_markup=markup)
+    if(message_type == 'cinema' or message_type == 'back_to_cinema'):
+        user.movie_name = message
+        user.movie_id = get_movie_id(user.city_id, message)
+        markup = draw_cinema_list(user.city_id)
+        bot.edit_message_text('Movie Bot', call.from_user.id, call.message.message_id, reply_markup=markup)
+    if(message_type == 'sessions'):
+        user.cinema_name = message
+        user.Cinema_id = get_cinema_id(user.city_id, user.cinema_name)
+        markup = draw_seesions_list(user.city_id, user.movie_id, user.cinema_id, user.cinema_name)
+        bot.edit_message_text('Movie Bot', call.from_user.id, call.message.message_id, reply_markup=markup)
